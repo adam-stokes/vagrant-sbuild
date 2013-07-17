15c8bbb7ff47df648412b092f3d6909ac3275d41
Adam Stokes <adam.stokes@ubuntu.com>
2013-07-17 15:03:06 -0400

    start sbuild puppet
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ puppet/modules/sbuild/manifests/init.pp
@@ -0,0 +1,45 @@
+class sbuild::install{
+    package{
+      "sbuild": ensure => present,
+      "debhelper": ensure => present,
+      "ubuntu-dev-tools": ensure => present
+    }
+
+    exec { "adduser":
+      command => "adduser vagrant sbuild",
+      path => "/usr/sbin"
+    }
+
+    file_line { 'schroot_mount':
+      path => '/etc/schroot/sbuild/fstab'
+      line => '/home/vagrant/ubuntu/scratch  /scratch          none  rw,bind  0  0'
+    }
+
+    file {
+      '/home/vagrant/.sbuildrc':
+      owner => 'vagrant',
+      group => 'vagrant',
+      ensure => file,
+      mode => 644,
+      source => '/vagrant/files/sbuildrc',
+      require => Package["sbuild"],
+    }
+
+    file {
+      '/home/vagrant/.mk-sbuild.rc':
+      owner => 'vagrant',
+      group => 'vagrant',
+      ensure => file,
+      mode => 644,
+      source => '/vagrant/files/mk-sbuild.rc'
+      require => Package["sbuild"],
+    }
+
+    file {
+      '/home/vagrant/ubuntu/logs':
+      ensure => 'directory',
+      owner => "vagrant",
+      group => "vagrant",
+      mode => 750,
+    }
+}
